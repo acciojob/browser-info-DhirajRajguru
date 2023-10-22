7c6972098d215a0a882b23a572c016af7b4329d5
DhirajRajguru <110520184+DhirajRajguru@users.noreply.github.com>
2023-10-22 23:17:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+function getBrowserInfo() {
+    const userAgent = navigator.userAgent;
+    let browserName;
+    let version;
+
+   
+    if (userAgent.includes("Chrome")) {
+        browserName = "Google Chrome";
+        version = userAgent.match(/Chrome\/(\d+\.\d+)/)[1];
+    }
+    // Check for Firefox
+    else if (userAgent.includes("Firefox")) {
+        browserName = "Mozilla Firefox";
+        version = userAgent.match(/Firefox\/(\d+\.\d+)/)[1];
+    }
+    // Check for Safari
+    else if (userAgent.includes("Safari")) {
+        browserName = "Apple Safari";
+        version = userAgent.match(/Version\/(\d+\.\d+)/)[1];
+    }
+    // Check for Edge
+    else if (userAgent.includes("Edg")) {
+        browserName = "Microsoft Edge";
+        version = userAgent.match(/Edg\/(\d+\.\d+)/)[1];
+    }
+    
+    else {
+        browserName = "Unknown Browser";
+        version = "";
+    }
+
+    const browserInfo = `You are using ${browserName} version ${version}`;
+    const browserInfoDiv = document.getElementById("browser-info");
+    browserInfoDiv.textContent = browserInfo;
+}
+
+
+getBrowserInfo();
